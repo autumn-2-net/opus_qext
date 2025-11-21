e8f09ae7778768c027d2ab1060b3732b36cd4c5f
autumn <109412646+autumn-2-net@users.noreply.github.com>
2025-11-21 20:45:20 +0800

    fix linux support

--- win32_compat/opusfile_stub.c
+++ win32_compat/opusfile_stub.c
@@ -1,11 +1,11 @@
 /* Stub for opusfile HTTP functions when HTTP support is disabled */
 #include <opusfile.h>
+#include <stdarg.h>
 
 /* Stub for op_open_url when HTTP support is not available */
-OggOpusFile *op_open_url(const char *url, int *error, const OpusServerInfo *info) {
+OggOpusFile *op_open_url(const char *url, int *error, ...) {
     /* Always fail - HTTP support not compiled in */
     if (error) *error = OP_ENOTFORMAT;
     (void)url;
-    (void)info;
     return NULL;
 }
